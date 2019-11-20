72de558749264ef5f582b86939950bad8f667b27
Jake Litwicki <jake.litwicki@gmail.com>
2019-11-19 16:29:27 -0800

    async writes in redux module

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { connect } from 'react-redux';
-import * as courseActions from '../../redux/actions/courseActions';
-import * as authorActions from '../../redux/actions/authorActions';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
 
   componentDidMount() {
-
     const { courses, authors, actions } = this.props;
 
-    if(courses.length === 0) {
+    if (courses.length === 0) {
       actions.loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     }
 
-    if(authors.length === 0) {
+    if (authors.length === 0) {
       actions.loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
@@ -28,28 +31,41 @@ class CoursesPage extends React.Component {
   render() {
     return (
       <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
+
         <CourseList courses={this.props.courses} />
       </>
-    )
+    );
   }
 }
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state) {
   debugger;
   return {
-    courses: state.authors.length === 0 ? [] : state.courses.map(course => {
-      return {
-        ...course,
-        authorName: state.authors.find(a => a.id === course.authorId).name
-      };
-    }),
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
     authors: state.authors
   };
 }
